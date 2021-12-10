976163919a248037f7ef41501014b58104c89612
Victor Pinas Arnault <49802514+VictorPinas@users.noreply.github.com>
2021-12-10 16:11:31 -0300

    Add files via upload

---
+++ ArrayCopyOfDemo.java
@@ -0,0 +1,13 @@
+class ArrayCopyOfDemo {
+    public static void main(String[] args){
+        String[] copyFrom = {
+            "Affogato", "Americano", "Cappuccino", "Corretto", "Cortado",   
+             "Doppio", "Espresso", "Frappucino", "Freddo", "Lungo", "Macchiato",      
+             "Marocchino", "Ristretto"
+        };
+        String[] copyTo = java.util.Arrays.copyOfRange(copyFrom, 2, 9);
+        for (String coffee: copyTo) {
+            System.out.print(coffee + " ");
+        }
+    }
+}
