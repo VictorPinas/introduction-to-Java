c67f7f1405315b5605334b9e6f3f065936f6f4c3
Victor Pinas Arnault <victor.arnault@yahoo.com>
2022-07-23 18:18:51 -0300

    names for methods

--- carroThis.java
+++ carroThis.java
@@ -3,26 +3,26 @@ public class carroThis{
   private boolean ligar, desligar, farol, sinal;
   
   //operador this em um construtor
-  //public carroThis(){
-   //this(0, 0, 0, 0);
-   //this(false, false, false, false);
-  //}
+  public carroThis(){
+   this(0, 0, 0, 0);
+   this(false, false, false, false);
+  }
   
-  public carroThis(int velocidade, int aceleracao, int diminuir, int frear){
+  public movimento(int velocidade, int aceleracao, int diminuir, int frear){
     this(0, 0, 0, 0, velocidade, aceleracao, diminuir, frear);
   }
   
-  public carroThis2(boolean ligar, boolean desligar, boolean farol, boolean sinal){
+  public ignicao(boolean ligar, boolean desligar, boolean farol, boolean sinal){
     this(false, false, false, false, ligar, desligar, farol, sinal);
   }
   
-  public carroThis(int velocidade, int aceleracao, int diminuir, int frear){
+  public movimento(int velocidade, int aceleracao, int diminuir, int frear){
     this.velocidade = velocidade;
     this. aceleracao = aceleracao;
     this.diminuir = diminuir;
     this.frear = frear;
   }
-  public carroThis2(boolean ligar, boolean desligar, boolean farol, boolean sinal){
+  public ignicao(boolean ligar, boolean desligar, boolean farol, boolean sinal){
    this.ligar = ligar;
    this.desligar = desligar;
    this.farol = farol;
