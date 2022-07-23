e1669732ea9ac6b8c6676a4d8c78e6ca497e24ef
Victor Pinas Arnault <victor.arnault@yahoo.com>
2022-07-23 18:07:32 -0300

    changes

--- carroThis.java
+++ carroThis.java
@@ -3,16 +3,16 @@ public class carroThis{
   private boolean ligar, desligar, farol, sinal;
   
   //operador this em um construtor
-  public carroThis(){
-   this(0, 0, 0, 0);
-   this(false, false, false, false);
-  }
+  //public carroThis(){
+   //this(0, 0, 0, 0);
+   //this(false, false, false, false);
+  //}
   
   public carroThis(int velocidade, int aceleracao, int diminuir, int frear){
     this(0, 0, 0, 0, velocidade, aceleracao, diminuir, frear);
   }
   
-  public carroThis(boolean ligar, boolean desligar, boolean farol, boolean sinal){
+  public carroThis2(boolean ligar, boolean desligar, boolean farol, boolean sinal){
     this(false, false, false, false, ligar, desligar, farol, sinal);
   }
   
@@ -22,7 +22,7 @@ public class carroThis{
     this.diminuir = diminuir;
     this.frear = frear;
   }
-  public carroThis(boolean ligar, boolean desligar, boolean farol, boolean sinal){
+  public carroThis2(boolean ligar, boolean desligar, boolean farol, boolean sinal){
    this.ligar = ligar;
    this.desligar = desligar;
    this.farol = farol;
