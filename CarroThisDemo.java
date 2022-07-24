213540dbe48e8fc5ffd9b0a225fbc5eb65e6b8af
Victor Pinas Arnault <victor.arnault@yahoo.com>
2022-07-24 15:49:17 -0300

    Rename class

---
+++ CarroThisDemo.java
@@ -0,0 +1,10 @@
+public class CarroThisDemo{
+  public static void main(String[] args){
+    CarroThis carroThis = new CarroThis();
+    
+    carroThis.velocidade(80);
+    carroThis.aceleracao(10);
+    carroThis.diminuir(15);
+    carroThis.frear(0);
+  }
+}
