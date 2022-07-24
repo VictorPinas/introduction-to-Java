face041ce59b809c1b142d2a23f93c426f96a047
Victor Pinas Arnault <victor.arnault@yahoo.com>
2022-07-24 09:25:20 -0300

    compile time error corrections

--- carroThis.java
+++ carroThis.java
@@ -1,26 +1,27 @@
 public class carroThis{
-  public int velocidade, aceleracao, diminuir, frear;
+   public static void (int velocidade, aceleracao, diminuir, frear);
   
   //operador this em um construtor
-  public carroThis(){
-   this.velocidade = velocidade;
-   this.aceleracao = aceleracao;
-   this.diminuir = diminuir;
-   this.frear = frear;
-  }
+   public carroThis(){
+      this.velocidade = velocidade;
+      this.aceleracao = aceleracao;
+      this.diminuir = diminuir;
+      this.frear = frear;
+   }
   
   //public int movimento(int velocidade, int aceleracao, int diminuir, int frear){
     //this(0, 0, 0, 0, velocidade, aceleracao, diminuir, frear);
   //}
   
-  public int iniciarMovimento(int velocidade, int aceleracao, int diminuir, int frear){
-    this.velocidade = velocidade;
-    this. aceleracao = aceleracao;
-    this.diminuir = diminuir;
-    this.frear = frear;
-  }
-  return velocidade;
-  return aceleracao;
-  return diminuir;
-  return frear;  
+   public static void iniciarMovimento(int velocidade, int aceleracao, int diminuir, int frear){
+      this.velocidade = velocidade;
+      this. aceleracao = aceleracao;
+      this.diminuir = diminuir;
+      this.frear = frear;
+   }
+  
+   void exibirMovimento(){
+      System.out.println(velocidade + aceleracao + diminuir + frear);
+   }
+   //return exibirMovimento;
 }
