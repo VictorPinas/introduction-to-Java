21888d59acc2bc6d570d2317bebcd204a4461a16
Victor Pinas Arnault <victor.arnault@yahoo.com>
2023-03-03 11:48:48 -0300

    mais programas java

--- carroThis.java
+++ carroThis.java
@@ -1,22 +1,30 @@
-public class carroThis{
-   int velocidade;
-   int aceleracao;
+public class carroThis {
+   // campos
+   int velocidade = 0;
+   int aceleracao = 0;
    int diminuir;
    int frear;
-   
-  //operador this em um construtor
-   public carroThis(int velocidade, int aceleracao, int diminuir, int frear){
+
+   // operador this em um construtor
+   public carroThis(int velocidade, int aceleracao, int diminuir, int frear) {
+      // métodos
       this.velocidade = velocidade;
       this.aceleracao = aceleracao;
       this.diminuir = diminuir;
       this.frear = frear;
    }
-   public static void main(String[] args){
-    CarroThis carroThis = new carroThis(velocidade, aceleracao, diminuir, frear);
-    
-    System.out.println(carroThis.velocidade);
-    System.out.println(carroThis.aceleracao);
-    System.out.println(carroThis.diminuir);
-    System.out.println(carroThis.frear);
+
+   public static void main(String[] args) {
+      int velocidade = 100;
+      int aceleracao = 10;
+      int diminuir = 5;
+      int frear = 0;
+
+      carroThis carroThis = new carroThis(velocidade, aceleracao, diminuir, frear);
+
+      System.out.println(carroThis.velocidade);
+      System.out.println(carroThis.aceleracao);
+      System.out.println(carroThis.diminuir);
+      System.out.println(carroThis.frear);
    }
 }
---
+++ constantPI.java
@@ -0,0 +1,7 @@
+public class constantPI {
+    static final double PI = 3.141592653589793;
+
+    public static void main(String[] args) {
+        System.out.println("O valor de pi é: " + PI);
+    }
+}
